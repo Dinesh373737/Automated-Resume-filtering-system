055c664db6b05df3074ba2be8ae4a43348e87ce9
Dinesh373737 <dineshkulkarni377@gmail.com>
2025-09-21 18:46:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -601,5 +601,5 @@ def health_check():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render will provide PORT
+    port = int(os.environ.get("PORT", 7860))  # Render will provide PORT
     app.run(host="0.0.0.0", port=port)
